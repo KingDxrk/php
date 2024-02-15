ab0ce9a58069cef2174ab9031b7283b5f7983e10
KingDxrk <159971793+KingDxrk@users.noreply.github.com>
2024-02-15 01:33:02 +0100

    tarea1.2
    xd

--- xd.php
+++ xd.php
@@ -6,13 +6,20 @@
 $x=5;
 $y=3;
 echo "Valores x, y: ", $x, " ", $y , "<br>";
-$x+=$y; //equivale a $x=$y+$x
+$x+=$y; //EQUIVALE A: $x=$y+$x
 echo "x: ", $x, "<br>";
 $x-=$y;
-echo "x: ", $x, "<br>"; //x vale s
+echo "x: ", $x, "<br>"; //X VALE S
 $a=$x++;
 echo "a x: ", $a, " ", $x, "<br>";
+$a=++$x;
+echo "a x: ", $a, " ", $x, "<br>";
+if($a==$x and $x%2==1)
+{
+    echo "CUMPLE CONDICION";
+}
+ECHO "ENCUENTRA EL MAXIMO: <br>";
+echo max(2,3,12,698,7);
     ?>
 </body>
-
 </html>
